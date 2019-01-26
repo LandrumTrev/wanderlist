6a3f3f8997387f720512d877c0186da975e0b8cf
LandrumTrev <landrum.trevillian@gmail.com>
2019-01-26 02:32:51 -0500

    first build

---
+++ client/build/precache-manifest.f2a1ab10252642a490be0db6ec01aefc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5b7490021123acde36a2",
+    "url": "/static/js/main.5b749002.chunk.js"
+  },
+  {
+    "revision": "495ef4dcf3e5597b600e",
+    "url": "/static/js/1.495ef4dc.chunk.js"
+  },
+  {
+    "revision": "5b7490021123acde36a2",
+    "url": "/static/css/main.6972b270.chunk.css"
+  },
+  {
+    "revision": "b5caaa09de2e60a6933ef299da4b4283",
+    "url": "/index.html"
+  }
+];
