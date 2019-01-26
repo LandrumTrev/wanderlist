b912917b421f4d04b81b2c8f704c8c700c1c031b
LandrumTrev <landrum.trevillian@gmail.com>
2019-01-26 18:38:18 -0500

    routes index comments

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,20 @@
+// MAIN ROUTER: ALL ROUTES START HERE (required by express server.js)
+
+// path.join used to attach domain to HTML path for React (production)
 const path = require("path");
+// Express Router has routing info attached to it
 const router = require("express").Router();
+// require all API routes
 const apiRoutes = require("./api");
 
-// API Routes
+// all API routes attached to Express Router
 router.use("/api", apiRoutes);
 
-// If no API routes are hit, send the React app
+// (production only) all HTML routes sent to React client production build
+// HTML routes are all other routes that don't start with /api
 router.use(function(req, res) {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
+// make routes attached as router.use() available to server
 module.exports = router;
