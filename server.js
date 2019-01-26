28a13cc1ef96a65ebfcdf9ecfc4b9daf4f4d7686
LandrumTrev <landrum.trevillian@gmail.com>
2019-01-26 14:27:42 -0500

    server comments

--- server.js
+++ server.js
@@ -1,24 +1,32 @@
+// express web server for node.js
 const express = require("express");
-
-const mongoose = require("mongoose");
-const routes = require("./routes");
+// app is express server invoked
 const app = express();
+// mongoose ODM interface for MongoDB
+const mongoose = require("mongoose"); 
+// all API and HTML routes
+const routes = require("./routes");
+// production OR local PORT
 const PORT = process.env.PORT || 3001;
 
-// Define middleware here
+// express server can use URLs encoded with other data
 app.use(express.urlencoded({ extended: true }));
+// express server can handle JSON data
 app.use(express.json());
-// Serve up static assets (usually on heroku)
+
+// for production builds, get static files from /client/build/
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
-// Add routes, both API and view
+
+// express server uses HTML and API routes 
+// defined in routes/index.js
 app.use(routes);
 
-// Connect to the Mongo DB
+// mongoose connects to MongoDB (production OR local)
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
 
-// Start the API server
+// express server listening on PORT (localhost:3001)
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
