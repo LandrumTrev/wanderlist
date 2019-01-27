beb29ba62aafba5c6f4f0de8d02f698788518929
LandrumTrev <landrum.trevillian@gmail.com>
2019-01-26 20:16:43 -0500

    user auth routes added

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,8 +3,26 @@
 
 // include Express Router to attach routes to it
 const router = require("express").Router();
+
+// controller for user authentication routes methods
+const loginController = require("../../controllers/loginController");
+
 // require all routes for /api/books/xxx
 const bookRoutes = require("./books");
+
+// api routes for user authentication
+router
+  .route("/signup")
+  .post(loginController.signUp);
+router
+  .route("/signin")
+  .post(loginController.signIn);
+router
+  .route("/verify")
+  .post(loginController.verify);
+router
+  .route("/logout")
+  .post(loginController.logout);
 
 // attach /api/books/xxx routes to Express Router
 router.use("/books", bookRoutes);
