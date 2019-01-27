f3f44d9689e5fbd6d1971638866b283c14f43e51
LandrumTrev <landrum.trevillian@gmail.com>
2019-01-27 13:54:53 -0500

    create new user operational

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -10,9 +10,13 @@ class Nav extends Component {
       show: false,
       firstName: "",
       lastName: "",
+      newEmail: "",
+      newPassword: "",
       email: "",
       password: "",
-      isDeleted: false
+      isDeleted: false,
+      isLoggedIn: false,
+      loginMsg: ""
     };
   }
 
@@ -32,13 +36,13 @@ class Nav extends Component {
 
   componentDidUpdate() {
     console.log(this.state);
-    console.log("Nav component updated");
+    // console.log("Nav component updated");
   }
   // ===================================================
 
   componentDidMount() {
     console.log(this.state);
-    console.log("Nav component mounted");
+    // console.log("Nav component mounted");
   }
   // ===================================================
 
@@ -48,63 +52,100 @@ class Nav extends Component {
       [name]: value
     });
     // console.log("name = " + name);
-    // console.log("value = " + value);  
+    // console.log("value = " + value);
   };
 
   // ===================================================
 
   loginUser = event => {
-    // console.log("user login!");
-    event.preventDefault();
-    this.validateUser({
-      email: this.state.loginEmail,
-      password: this.state.loginPassword
-    });
-    // alert("You may or may not have been logged in!");
-    this.setState({ show: false });
+    console.log("user login!");
+    // event.preventDefault();
+    // this.validateUser({
+    //   email: this.state.loginEmail,
+    //   password: this.state.loginPassword
+    // });
+    // // alert("You may or may not have been logged in!");
+    // this.setState({ show: false });
   };
 
-  createUser = event => {
-    event.preventDefault();
-    this.validateUser({
-      email: this.state.loginEmail,
-      password: this.state.loginPassword
-    });
-    // console.log("state = " + JSON.stringify(this.state));  
-    // console.log("user create!");    
-    // alert("You may or may not have had a new account created!");
-    // alert(`${this.state.firstName}, ${this.state.lastName}, ${this.state.email}, ${this.state.password}`);
-    this.setState({ show: false });
-  };
+  // validateUser = query => {
+  //   // getUser calls /api/signin route to log in existing user
+  //   // (query) is an object containing entered email and password
+  //   API.getUser(query)
+  //     .then(res => {
+  //       console.log("LOGIN: res = " + JSON.stringify(res));
+  //       if (res.data.success) {
+  //         console.log("in success handle");
+  //         this.setState({ isLoggedIn: true });
+  //         this.setState({ loginMsg: res.data.message });
+  //         window.localStorage.setItem("Wanderlist_authkey", res.data.token);
+  //         window.location.assign("/authenticated/main");
+  //       } else {
+  //         console.log("in failure handle");
+  //         this.setState({ isLoggedIn: false });
+  //         this.setState({ loginMsg: res.data.message });
+  //         window.localStorage.setItem("Wanderlist_authkey", "");
+  //         window.location.assign("/login");
+  //       }
+  //       console.log("LOGIN: state = " + JSON.stringify(this.state));
+  //     })
+  //     .catch(err => console.log(err));
+  // };
+
+    // ===================================================
+
+    createUser = event => {
+      // console.log("start creating user!");
+      event.preventDefault();
+      // console.log(`${this.state.firstName}, ${this.state.lastName}, ${this.state.newEmail}, ${this.state.newPassword}`);
+      this.makeNewUser({
+        firstName: this.state.firstName,
+        lastName: this.state.lastName,
+        email: this.state.newEmail,
+        password: this.state.newPassword
+      });
+      // console.log("state = " + JSON.stringify(this.state));
+      // alert("You may or may not have had a new account created!");
+      this.setState({ show: false });
+    };
   
-  validateUser = query => {
-    API.getUser(query)
-      .then(res => {
-        console.log("LOGIN: res = " + JSON.stringify(res));
-        if (res.data.success) {
-          console.log("in success handle");
-          this.setState({ isLoggedIn: true, });
-          this.setState({ loginMsg: res.data.message });
-          window.localStorage.setItem("SMC_authkey", res.data.token);
-          window.location.assign('/authenticated/main');
-        } else {
-          console.log("in failure handle");
-          this.setState({ isLoggedIn: false });
-          this.setState({ loginMsg: res.data.message });
-          window.localStorage.setItem("SMC_authkey", "");
-          window.location.assign('/login');
-        }
-        console.log("LOGIN: state = " + JSON.stringify(this.state));
-      })
-      .catch(err => console.log(err));
-  };
+    makeNewUser = query => {
+      // createUser calls /api/signup route to create a new user
+      // (query) is an object entered firstName, lastName, email and password
+      console.log("this is the (query) to makeNewUser():");
+      console.log(query);
+      API.createUser(query)
+        .then(res => {
+          console.log("SIGNUP: res = " + JSON.stringify(res));
+          if (res.data.success) {
+            console.log("in success handle");
+      //       this.setState({ isLoggedIn: true });
+      //       this.setState({ loginMsg: res.data.message });
+      //       // set Wanderlist_authkey key in Local Storage, value is token
+      //       window.localStorage.setItem("Wanderlist_authkey", res.data.token);
+      //       // then send user to /authenticated/main page view
+      //       window.location.assign("/authenticated/main");
+          } else {
+            console.log("in failure handle");
+      //       this.setState({ isLoggedIn: false });
+      //       this.setState({ loginMsg: res.data.message });
+      //       // set Wanderlist_authkey key in Local Storage, value is empty (false)
+      //       window.localStorage.setItem("Wanderlist_authkey", "");
+      //       // then send user to /login page view
+      //       window.location.assign("/login");
+          }
+      //     console.log("LOGIN: state = " + JSON.stringify(this.state));
+        })
+        .catch(err => console.log(err));
+    };
+  
   
   // ===================================================
 
   render() {
     return (
       <>
-        <Modal 
+      <Modal 
           show={this.state.show} 
           handleClose={this.hideModal} 
           handleLogin={this.loginUser} 
@@ -141,17 +182,17 @@ class Nav extends Component {
                 name="lastName" 
                 placeholder="last name (required)" />
                 <Input 
-                value={this.state.email} 
+                value={this.state.newEmail} 
                 onChange={this.handleInputChange} 
-                name="email" 
+                name="newEmail" 
                 placeholder="email (required)" />
               <Input 
-                value={this.state.password} 
+                value={this.state.newPassword} 
                 onChange={this.handleInputChange} 
-                name="password" 
+                name="newPassword" 
                 placeholder="password (required)" />
               <FormBtn 
-                disabled={!(this.state.firstName && this.state.lastName && this.state.email && this.state.password)} 
+                disabled={!(this.state.firstName && this.state.lastName && this.state.newEmail && this.state.newPassword)} 
                 onClick={this.createUser}>
                 Create Account
               </FormBtn>
