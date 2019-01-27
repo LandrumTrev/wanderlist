e412b976a1e866abd66a645bc9cb826db89b9baf
LandrumTrev <landrum.trevillian@gmail.com>
2019-01-26 19:25:12 -0500

    revise api routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,35 +3,11 @@
 
 // include Express Router to attach routes to it
 const router = require("express").Router();
-// require the controller methods for user authentication /api/xxx routes
-const loginController = require("../controllers/loginController");
 // require all routes for /api/books/xxx
 const bookRoutes = require("./books");
 
 // attach /api/books/xxx routes to Express Router
 router.use("/books", bookRoutes);
-
-// attach all user authentication /api/xxx routes
-// /api/signup route creates a new User if email does not exist in db "users"
-router
-  .route("/signup")
-  .post(loginController.signUp);
-
-// /api/signin route uses bcrypt to match login info hash with db User info hash
-router
-  .route("/signin")
-  .post(loginController.signIn);
-
-// /api/verify route allows component to check for UserSession isDeleted: false
-router
-  .route("/verify")
-  .post(loginController.verify);
-
-// /api/logout route sets UserSession model's isDeleted: true
-router
-  .route("/logout")
-  .post(loginController.logout);
-
 
 // export router for use by main router: routes/index.js
 module.exports = router;
