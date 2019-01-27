f8c7e4ecce235a7dc498752a7b277f6c420b747b
LandrumTrev <landrum.trevillian@gmail.com>
2019-01-26 21:54:02 -0500

    added state to Nav

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,20 +1,45 @@
-import React from "react";
+import React, { Component } from "react";
 
-function Nav() {
-  return (
-    <nav className="navbar navbar-expand-sm navbar-light bg-light">
-      <a className="navbar-brand" href="/">
-        {/* <img src="assets/images/dinofii_logo.svg" style={{ width: "30", height: "30" }} className="d-inline-block align-top" alt="" /> */}
-        Wanderlist
-      </a>
-      <a className="nav-link" href="/">
-        Saved
-      </a>
-      <a className="nav-link" href="/">
-        Login
-      </a>
-    </nav>
-  );
+class Nav extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      item: "value"
+    };
+  }
+
+  // ===================================================
+
+  componentDidUpdate() {
+    // console.log(this.state);
+    console.log("Nav component updated");
+  }
+  // ===================================================
+
+  componentDidMount() {
+    // console.log(this.state);
+    console.log("Nav component mounted");
+  }
+  // ===================================================
+
+  // ===================================================
+
+  render() {
+    return (
+      <nav className="navbar navbar-expand-sm navbar-light bg-light">
+        <a className="navbar-brand" href="/">
+          {/* <img src="assets/images/dinofii_logo.svg" style={{ width: "30", height: "30" }} className="d-inline-block align-top" alt="" /> */}
+          Wanderlist
+        </a>
+        <a className="nav-link" href="/">
+          Saved
+        </a>
+        <a className="nav-link" href="/">
+          Login
+        </a>
+      </nav>
+    );
+  }
 }
 
 export default Nav;
