ee88c6317354bf121927d587aff02dd0d5618d7a
LandrumTrev <landrum.trevillian@gmail.com>
2019-01-26 20:50:48 -0500

    basic navbar

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -2,9 +2,16 @@ import React from "react";
 
 function Nav() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+    <nav className="navbar navbar-expand-sm navbar-light bg-light">
       <a className="navbar-brand" href="/">
-        React Reading List
+        {/* <img src="assets/images/dinofii_logo.svg" style={{ width: "30", height: "30" }} className="d-inline-block align-top" alt="" /> */}
+        Wanderlist
+      </a>
+      <a className="nav-link" href="/">
+        Saved
+      </a>
+      <a className="nav-link" href="/">
+        Login
       </a>
     </nav>
   );
