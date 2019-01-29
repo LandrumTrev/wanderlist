b2c26403ae59a9aec04782a657e9685bc5e8ea28
LandrumTrev <landrum.trevillian@gmail.com>
2019-01-29 13:31:43 -0500

    navigation styling

--- client/src/App.js
+++ client/src/App.js
@@ -3,22 +3,23 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 // import Home from "./pages/Home";
 import FindPlace from "./pages/FindPlace";
 import Saved from "./pages/Saved";
-import Books from "./pages/Books";
-import Detail from "./pages/Detail";
-import NoMatch from "./pages/NoMatch";
-// import Nav from "./components/Nav";
+// import Books from "./pages/Books";
+// import Detail from "./pages/Detail";
+// import NoMatch from "./pages/NoMatch";
 
 function App() {
   return (
     <Router>
       <div>
-        {/* <Nav /> */}
         <Switch>
-          <Route exact path="/" component={FindPlace} />
+        <Route exact path="/" component={FindPlace} />
+        <Route exact path="/search" component={FindPlace} />
           <Route exact path="/saved" component={Saved} />
-          <Route exact path="/books" component={Books} />
-          <Route exact path="/books/:id" component={Detail} />
-          <Route component={NoMatch} />
+          <Route path="*" component={FindPlace} />
+          {/* <Route exact path="*" component={FindPlace} /> */}
+          {/* <Route exact path="/books" component={Books} /> */}
+          {/* <Route exact path="/books/:id" component={Detail} /> */}
+          {/* <Route component={NoMatch} /> */}
         </Switch>
       </div>
     </Router>
--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,34 +1,64 @@
 import React from "react";
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import "./style.css";
 
 function Nav(props) {
   // console.log(props);
 
   return (
-    <>
-      <nav className="navbar navbar-expand-sm navbar-light bg-light">
-        <a id="link-search" className="nav-link" href="/">
-          Search
-        </a>
+    <div>
+      <div className="card-group flex-nowrap">
+        <div className="card flex-fill m-0">
+          <NavLink
+            id="link-search-box"
+            className="nav-link nav-item"
+            to="/search"
+            style={{ color: "#999", backgroundColor: "aliceblue" }}
+            activeStyle={{ color: "aliceblue", backgroundColor: "#999" }}
+          >
+            <div className="card-body p-0">
+              <h4 className="card-title mb-0 text-center">SEARCH</h4>
+            </div>
+          </NavLink>
+        </div>
+
         {/* if isLoggedIn is set to true, then */}
         {props.loginStatus ? (
           <>
-            <a id="link-saved" className="nav-link" href="/saved">
-              Saved
-            </a>
-            <span id="link-logout" className="nav-link" onClick={props.logoutClick}>
-              Log Out
-            </span>
+            <div className="card flex-fill m-0">
+              <NavLink
+                id="link-saved-box"
+                className="nav-link nav-item"
+                to="/saved"
+                style={{ color: "#999", backgroundColor: "aliceblue" }}
+                activeStyle={{ color: "aliceblue", backgroundColor: "#999" }}
+              >
+                <div className="card-body p-0">
+                  <h4 className="card-title mb-0 text-center">SAVED</h4>
+                </div>
+              </NavLink>
+            </div>
+
+            <div className="card flex-fill m-0">
+              <span id="link-logout-box" className="nav-link nav-item" onClick={props.logoutClick}>
+                <div className="card-body p-0">
+                  <h4 className="card-title mb-0 text-center">LOG OUT</h4>
+                </div>
+              </span>
+            </div>
           </>
         ) : (
-          <span id="link-login" className="nav-link" onClick={props.modalPops}>
-            Log In
-          </span>
+          <div className="card flex-fill m-0">
+            <span id="link-login-box" className="nav-link nav-item" onClick={props.modalPops}>
+              <div className="card-body p-0">
+                <h4 className="card-title mb-0 text-center">LOG IN</h4>
+              </div>
+            </span>
+          </div>
         )}
-        {/* if isLoggedIn is set to true, then */}
-        {props.firstName ? <span id="link-username">{`Hey ${props.firstName}!`}</span> : <span />}
-      </nav>
-    </>
+      </div>
+    </div>
   );
 }
 
