ded1d08594300aa1cfc539833f31cc8faffe768c
LandrumTrev <landrum.trevillian@gmail.com>
2019-01-29 14:34:28 -0500

    basic login modal styling

--- client/src/components/Modal/index.js
+++ client/src/components/Modal/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import "./style.css";
 
-const Modal = ({ handleClose, handleLogin, handleNewUser, show, children }) => {
+const Modal = ({ handleClose, show, children }) => {
   const showHideClassName = show ? "login-modal display-block" : "login-modal display-none";
 
   return (
     <div className={showHideClassName}>
       <section className="login-modal-main">
-        <button id="close-button" onClick={handleClose}>x</button>
+        <button id="close-button" onClick={handleClose}><span>&times;</span></button>
         {children}
       </section>
     </div>
